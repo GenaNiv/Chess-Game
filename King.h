ba8277f5813207f0732c94fd4a6146a30185d1be
Gena <genadiniv@gmail.com>
2015-11-30 19:44:19 -0400

    Create King.h

---
+++ King.h
@@ -0,0 +1,16 @@
+/*
+ * King.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+//----------King's move validation-------------------
+#ifndef KING_H_
+#define KING_H_
+
+//---Function declaration---
+int King(char AttakedFigure,int x1,int y1,int x2,int y2);
+
+
+
+#endif /* KING_H_ */
