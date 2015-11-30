94415ad581233a5f57394fa62302312a68b50c00
Gena <genadiniv@gmail.com>
2015-11-30 19:50:37 -0400

    Create set_coords.h

---
+++ set_coords.h
@@ -0,0 +1,15 @@
+/*
+ * set_coords.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+
+#ifndef SET_COORDS_H_
+#define SET_COORDS_H_
+
+
+int set_coords();
+
+
+#endif /* SET_COORDS_H_ */
