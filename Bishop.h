c30643cb9d41c90a1b772f139586e65b4cfa4ebc
Gena <genadiniv@gmail.com>
2015-11-30 19:42:45 -0400

    Create Bishop.h

---
+++ Bishop.h
@@ -0,0 +1,14 @@
+/*
+ * Bishop.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+//---------Bishop's move validation-------
+#ifndef BISHOP_H_
+#define BISHOP_H_
+
+//---Function declaration---
+int Bishop(char AttakedFigure,int x1,int y1,int x2,int y2);
+
+#endif /* BISHOP_H_ */
