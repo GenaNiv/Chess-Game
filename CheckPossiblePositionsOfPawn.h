1ceacb5a69debb582ea3331c7fb2db97c1120dfb
Gena <genadiniv@gmail.com>
2015-11-30 19:51:33 -0400

    Create CheckPossiblePositionsOfPawn.h

---
+++ CheckPossiblePositionsOfPawn.h
@@ -0,0 +1,15 @@
+/*
+ * CheckPossiblePositionsOfPawn.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+
+#ifndef CHECKPOSSIBLEPOSITIONSOFPAWN_H_
+#define CHECKPOSSIBLEPOSITIONSOFPAWN_H_
+
+
+int CheckPossiblePositionsOfPawn(int x1,int y1,int x2,int y2); //Function checks whether all possible positions are empty
+
+
+#endif /* CHECKPOSSIBLEPOSITIONSOFPAWN_H_ */
