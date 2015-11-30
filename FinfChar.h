6811f963d121bcb9d2d0d99ccb6805856f983859
Gena <genadiniv@gmail.com>
2015-11-30 19:52:45 -0400

    Create FinfChar.h

---
+++ FinfChar.h
@@ -0,0 +1,14 @@
+/*
+ * FinfChar.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+
+#ifndef FINDCHAR_H_
+#define FINDCHAR_H_
+
+//---Function declaration---
+char FindChar(void);
+
+#endif /* FINDCHAR_H_ */
