ba532e6c14604ff03f87800d8c92c8c89b6409b7
Gena <genadiniv@gmail.com>
2015-11-30 19:46:43 -0400

    Create Pawn.h

---
+++ Pawn.h
@@ -0,0 +1,15 @@
+/*
+ * Pawn.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+// -----Pawn's move validation---------
+#ifndef PAWN_H_
+#define PAWN_H_
+
+//---Function declaration---
+int Pawn(char AttakedFigure,int x1,int y1,int x2,int y2);
+
+
+#endif /* PAWN_H_ */
