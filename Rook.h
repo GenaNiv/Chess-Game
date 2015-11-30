aa4c89c8cbea303187f67c0f2113b204d293c9e0
Gena <genadiniv@gmail.com>
2015-11-30 19:49:10 -0400

    Create Rook.h

---
+++ Rook.h
@@ -0,0 +1,14 @@
+/*
+ * Rook.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+//---------Rook's move validation---------
+#ifndef ROOK_H_
+#define ROOK_H_
+
+//---Function declaration---
+int Rook(char AttakedFigure,int x1,int y1,int x2,int y2);
+
+#endif /* ROOK_H_ */
