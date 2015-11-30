295ece1f85352be1b4389c793232299932a3cd29
Gena <genadiniv@gmail.com>
2015-11-30 19:45:42 -0400

    Create Knigth.h

---
+++ Knigth.h
@@ -0,0 +1,13 @@
+/*
+ * Knight.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+//-----Knigth's move validation------------
+#ifndef KNIGTH_H_
+#define KNIGTH_H_
+//---Function declaration---
+int Knigth(char AttakedFigure,int x1,int y1,int x2,int y2);
+
+#endif /* KNIGTH_H_ */
