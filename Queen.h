49fdda1054deb87be5922d0697bee0753cd113eb
Gena <genadiniv@gmail.com>
2015-11-30 19:47:37 -0400

    Create Queen.h

---
+++ Queen.h
@@ -0,0 +1,13 @@
+/*
+ * Queen.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: gena
+ */
+//--------Queen's move validation---------
+#ifndef QUEEN_H_
+#define QUEEN_H_
+//---Function declaration---
+int Queen(int x1,int y1,int x2,int y2);
+
+#endif /* QUEEN_H_ */
